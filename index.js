468f1ff2c96055d7bda5fd1b563ecadedf4e38ee
3barney <brnbsjm@gmail.com>
2018-10-14 15:40:34 +0300

    add payload handling for req.body items

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 const http  = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder; 
 
 const server = http.createServer(function(req, res) {
 
@@ -20,12 +21,32 @@ const server = http.createServer(function(req, res) {
 	// Get http method
 	var method = req.method.toLowerCase();
 
-	// Send the response
-	res.end("Hello World \n");
+	// Get Header as an object
+	var headers = req.headers
 
+	// Get payloads if any
+	var decoder = new StringDecoder('utf-8')
+	var buffer = ''
+
+	// As payload is streamed, req object emits data event, we subscribe to that to receive the data
+	req.on('data', function(data) {
+		buffer += decoder.write(data)
+	})
+
+	// When streaming ends, req object emits end event, (ALWAYS CALLED), after this we send our response back
+	req.on('end', function() {
+		buffer += decoder.end()
+
+		// Send the response
+		res.end("Hello World \n");
+	
+		console.log('Request received with payload', buffer)
+	})
+	
 	// Log the request path
-	console.log('Path is '+ trimmedPath + ' method '+ method)
-	console.log('Query String params ', queryStringObject)
+	// console.log('Path is '+ trimmedPath + ' method '+ method)
+	// console.log('Query String params ', queryStringObject)
+	// console.log('Headers received ', headers)
 });
 
 // Start server
