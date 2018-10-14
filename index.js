ebe5ba3451eea6104f9ee05d19041c8bc2df9ce5
3barney <brnbsjm@gmail.com>
2018-10-14 15:01:43 +0300

    get query string object items from url and requested method

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- *
+ * Entry File to the path
  */
 
 const http  = require('http');
@@ -14,11 +14,18 @@ const server = http.createServer(function(req, res) {
 	const path = parsedUrl.pathname
 	const trimmedPath = path.replace(/^\/+|\/+$/g, '');	// Esure foo/ equals to foo ==> remove last slashes
 
+	// Get query String objects from url
+	var queryStringObject = parsedUrl.query;
+
+	// Get http method
+	var method = req.method.toLowerCase();
+
 	// Send the response
 	res.end("Hello World \n");
 
 	// Log the request path
-	console.log('Path is '+ trimmedPath)
+	console.log('Path is '+ trimmedPath + ' method '+ method)
+	console.log('Query String params ', queryStringObject)
 });
 
 // Start server
